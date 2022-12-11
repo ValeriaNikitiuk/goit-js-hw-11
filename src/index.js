4dc8c825587f4f201cb25d2e30b3cb339c1ccb91
ValeriaOvod <krossovki.ukraine.vzutya@gmail.com>
2022-12-11 16:25:03 +0200

    ready homework

--- src/index.js
+++ src/index.js
@@ -1,22 +1,53 @@
 import  onFetch  from './js/fetch.js';
+import SimpleLightbox from 'simplelightbox';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { onRenderList } from './js/renderList.js';
 
-const form = document.querySelector('.search-form');
+// const form = document.querySelector('.search-form');
+let numberPage = 1;
 const galery = document.querySelector('.gallery');
 const searchBtn = document.querySelector('.search-btn');
-let pageNumber = 1;
+const input = document.querySelector('.input');
 const loadMoreBtn = document.querySelector('.load-more__btn');
 loadMoreBtn.style.display = 'none';
+let gallerySimpleLightbox = new SimpleLightbox('.gallery a');
 
-// , onClickSerach);
-// loadMoreBtn .addEventListener('click', onSubSerach);
 
-searchBtn.addEventListener('click', e => {
-  e.preventDeafault();
+
+searchBtn.addEventListener('click', onBtnSubmit);
+loadMoreBtn.addEventListener('click', onClickMore);
+
+function onBtnSubmit(e)  {
+  e.preventDefault();
   onClean();
-  const valueTrim = form.value.trim();
+  const valueTrim = input.value.trim();
   if (valueTrim !== '') {
-    onFetch(valueTrim, pageNumber)
+  onFetch(valueTrim, numberPage)
+    .then(foundData => {
+      if (foundData.hits.length === 0) {
+        Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      } else {
+        onRenderList(foundData.hits);
+        Notify.success(
+          "Hooray! We found totalHits images."
+        );
+        loadMoreBtn.style.display = 'block';
+          gallerySimpleLightbox.refresh();
+                
+      }
+    })
+}
+
+}
+
+function onClickMore() {
+  numberPage++;
+  const valueTrim = input.value.trim();
+  loadMoreBtn.style.display = 'none';
+  onFetch(valueTrim, numberPage)
       .then(foundData => {
         if (foundData.hits.length === 0) {
           Notify.failure(
@@ -28,48 +59,21 @@ searchBtn.addEventListener('click', e => {
             "Hooray! We found totalHits images."
           );
           loadMoreBtn.style.display = 'block';
-          gallerySimpleLightbox.refresh();
                  
         }
       })
   }
-});
 
 
 
 
-function onRenderList(images) {
-      console.log(images, 'images');
-    const markupImg = images.map(image => {
-        
-      console.log('img', image);
-        return `
-    <div class="photo-card">
- <a href="${image.largeImageURL}"><img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" /></a>
-  <div class="info">
-    <p class="info-item">
-      <b>Likes:${image.likes}</b>
-    </p>
-    <p class="info-item">
-      <b>Views:${image.views}</b>
-    </p>
-    <p class="info-item">
-      <b>Comments:${image.comments}</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads:${image.downloads}</b>
-    </p>
-  </div>
-</div>`;
-    }).join('')
-    galery.innerHTML += markupImg;
-    
-}
+
+
 
 
 
 function onClean() {
-  pageNumber = 1;
+  numberPage = 1;
   galery.innerHTML = '';
   loadMoreBtn.style.display = 'none' 
 }
---
+++ src/js/renderList.js
@@ -0,0 +1,28 @@
+export function onRenderList(images) {
+    const galery = document.querySelector('.gallery');
+      console.log(images, 'images');
+    const markupImg = images.map(image => {
+        
+      console.log('img', image);
+        return `
+    <div class="photo-card">
+ <a href="${image.largeImageURL}"><img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" /></a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes:${image.likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views:${image.views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments:${image.comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads:${image.downloads}</b>
+    </p>
+  </div>
+</div>`;
+    }).join('')
+    galery.innerHTML += markupImg;
+    
+}
